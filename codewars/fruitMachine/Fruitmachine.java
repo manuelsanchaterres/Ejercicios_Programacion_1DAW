000a87c2691ac9bfa30fda8866d55bbe824f2e37
manuelsanchaterres <manuelsanchaterres@gmail.com>
2024-01-24 20:50:19 +0100

    kata basic tests passed, keep working to pass random tests

--- codewars/fruitMachine/Fruitmachine.java
+++ codewars/fruitMachine/Fruitmachine.java
@@ -1,21 +1,23 @@
 package fruitMachine;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 class Fruitmachine {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		final String[][] reels = new String[][]{
-			new String[]{"Wild", "Star", "Bell", "Shell", "Seven",
-                "Cherry", "Bar", "King", "Queen", "Jack"},
-			new String[]{"Wild", "Star", "Bell", "Shell", "Seven",
-                "Cherry", "Bar", "King", "Queen", "Jack"},
-			new String[]{"Wild", "Star", "Bell", "Shell", "Seven",
-                "Cherry", "Bar", "King", "Queen", "Jack"}};
-                
-		final int[] spins = new int[]{1, 2, 0};
+			new String[]{"King", "Cherry", "Bar", "Jack", "Seven","Queen", "Star", "Shell", "Bell", "Wild"},
+			new String[]{"Bell", "Seven", "Jack", "Queen", "Bar","Star", "Shell", "Wild", "Cherry", "King"},
+			new String[]{"Wild", "King", "Queen", "Seven", "Star","Bar", "Shell", "Cherry", "Jack", "Bell"}
+			};
+			
+		//capturedReelItems = {"King","Bell","King"};
+		final int[] spins = new int[]{0, 0, 1};
 		
+		// totalScore 0
 		int totalScore = fruit(reels, spins);
 	}
 	
@@ -24,20 +26,26 @@ class Fruitmachine {
 	static int fruit(final String[][] reels, final int[] spins) {
 		
 		ArrayList <String> capturedReelItems = captureReelItem(reels,spins);
+		List<String> capturedReelItemsList = capturedReelItems;
 		int totalScore = 0;
-		int indexScoreTable = -1;
 		int totalMatches = compareItems(capturedReelItems);
-
-		if (totalMatches == 2) {
-			
-			totalScore = 2;
-			
-		}else if (totalMatches == 3) {
-			
-			totalScore = 3;
-			
-		}
+		String repeatedItemReel = findRepeatedReelItem(capturedReelItems);
 		
+		if (totalMatches == 2 ) {
+			
+			List<String> filteredList = capturedReelItemsList.stream()
+	                .filter(item -> item != repeatedItemReel)
+	                .collect(Collectors.toList());
+			// we get the non-repeated element of capturedReelItemsList List
+			
+			String nonRepeatedItemReel = filteredList.get(0);
+			
+			totalScore = calculateTotalScore(repeatedItemReel, nonRepeatedItemReel);
+			
+		} else if (totalMatches == 3) {
+			
+			totalScore = calculateTotalScore(repeatedItemReel);
+		}
 		return totalScore;
 	};
 	
@@ -69,7 +77,79 @@ class Fruitmachine {
 			}
 		}
 		
+		
 		return numberMatches;
 	}
 	
+	static String findRepeatedReelItem(ArrayList <String> capturedReelItems) {
+		
+		String repeatedItemReel= "";
+		
+		for (int i = 1; i < capturedReelItems.size(); i++) {
+			
+			if (capturedReelItems.get(i).equals(capturedReelItems.get(0))) { 
+				repeatedItemReel = capturedReelItems.get(i);
+				
+			}
+		}
+		
+		return repeatedItemReel;
+	}
+	
+	
+	static int calculateTotalScore (String repeatedItemReel, String nonRepeatedItemReel) {
+		String[] reels = {"Wild","Star","Bell","Shell","Seven","Cherry","Bar","King","Queen","Jack"};
+		int[][] scores = {
+		{100,10,0},
+		{90,9,18},
+		{80,8,16},
+		{70,7,14},
+		{60,6,12},
+		{50,5,10},
+		{40,4,8},
+		{30,3,6},
+		{20,2,4},
+		{10,1,2},
+		};
+		
+		int indexOfRepeatedItem = Arrays.asList(reels).indexOf(repeatedItemReel);
+		int indexOfArrayScoresLists = 0;
+		int totalScore = 0;
+		
+		
+			
+		if (nonRepeatedItemReel.equals("Wild")) {
+			
+			indexOfArrayScoresLists = 2;
+			totalScore = scores[indexOfRepeatedItem][indexOfArrayScoresLists];
+		} else {
+			indexOfArrayScoresLists = 1;
+			totalScore = scores[indexOfRepeatedItem][indexOfArrayScoresLists];
+		}
+			
+		return totalScore;
+	}
+	
+	static int calculateTotalScore (String repeatedItemReel) {
+		
+		String[] reels = {"Wild","Star","Bell","Shell","Seven","Cherry","Bar","King","Queen","Jack"};
+		int[][] scores = {
+		{100,10,0},
+		{90,9,18},
+		{80,8,16},
+		{70,7,14},
+		{60,6,12},
+		{50,5,10},
+		{40,4,8},
+		{30,3,6},
+		{20,2,4},
+		{10,1,2},
+		};
+		
+		int indexOfRepeatedItem = Arrays.asList(reels).indexOf(repeatedItemReel);
+		int indexOfArrayScoresLists = 0;
+		int totalScore = scores[indexOfRepeatedItem][indexOfArrayScoresLists];
+		
+		return totalScore;
+	}
 }
--- codewars/fruitMachine/Fruitmachine.md
+++ codewars/fruitMachine/Fruitmachine.md
@@ -23,12 +23,19 @@ Función fruit(final String[][] reels, final int[] spins):
 	int totalPuntos = 0;
 	int indiceTabla = -1;
 	int totalCoincidencias = compararItems(itemsReelCapturados);
-	
-	si totalCoincidencias es igual a 1 entonces:
-		Devolver totalPuntos
-	sino si totalCoincidencias es igual a 2 entonces:
-	
+	String reelItemRepetido = encontrarReelItemRepetido(itemsReelCapturados)
+	si totalCoincidencias es igual a 2 entonces:
+		filtrar array itemsReelCapturados para obtener el elemento String no repetido
+		
+		asignar elemento no repetido a variable string
+		
+		asignar valor de Funcion calcularTotalPuntos(reelItemRepetido, reelItemNoRepetido) a totalPuntos
+		
+		
 	sino si totalCoincidencias es igual a 3 entonces:
+		
+		asignar valor de Funcion calcularTotalPuntos(reelItemRepetido) a totalPuntos
+	Devolver totalPuntos;
 	
 Fin Función
 
@@ -41,15 +48,14 @@ Función capturarItemsReel(final String[][] reels, final int[] spins):
 		ACCEDEMOS A CADA String ARRAY DE REELS Y DESPUÉS CAPTURAMOS EL VALOR DE LA POSICIÓN I DE SPINS, QUE SERÁ EL INDICE DEL STRING A CAPTURAR EN REELS Y LO ASIGNAMOS A VARIABLE STRING reelItemCapturado
 		AÑADIMOS ITEM CAPTURADO A ARRAYLIST itemsReelCapturados
 		
-		
 	Devolver itemsReelCapturados
 	
 Fin Función
 
 Función compararItems(ArrayList <String> itemsReelCapturados):
 	
-	int numeroCoincidencias = 1;
-
+	int[] numeroCoincidencias = 1;
+	
 	Para i desde 1 hasta longitud itemsReelCapturados - 1:
 	
 		si itemsReelCapturados[i] es igual a itemsReelCapturados[0] entonces:
@@ -59,3 +65,60 @@ Función compararItems(ArrayList <String> itemsReelCapturados):
 	Devolver numeroCoincidencias
 			
 Fin Función
+
+Función encontrarReelItemRepetido(ArrayList <String> itemsReelCapturados):
+	
+	String reelItemRepetido = "";
+	
+	Para i desde 1 hasta longitud itemsReelCapturados - 1:
+	
+		
+			si itemsReelCapturados[i] es igual a itemsReelCapturados[0] entonces:
+			
+				asignar reelItemRepetido a itemsReelCapturados[i]
+				
+				Devolver itemsReelCapturados[i]
+				
+				
+	Devolver reelItemRepetido
+			
+Fin Función
+
+Función calcularPuntuacionTotal(String reelItemRepetido, String reelItemNoRepetido):
+	
+	String[] Reels = {"Wild","Star","Bell","Shell","Seven","Cherry","Bar","King","Queen","Jack"}
+	int[][] scores = {
+	{100,10,0},
+	{90,9,18},
+	{80,8,16},
+	{70,7,14},
+	{60,6,12},
+	{50,5,10},
+	{40,4,8},
+	{30,3,6},
+	{20,2,4},
+	{10,1,2},
+	}
+	int indiceDeItemRepetido se asigna el valor de indice de variable String reelItemRepetido dentro de array Reels
+	int indiceDeStringArrayScores = 1;
+	int totalScore = 0;
+	
+	// SI EL ITEM NO REPETIDO ES "Wild", LA PUNTUACION CORRESPONDE AL TERCER ELEMENTO DE CADA ARRAY DE SCORES
+	si reelItemNoRepetido es igual a "Wild" entonces:
+	
+		indiceDeStringArrayScores = 2;
+		
+		totalScore = VALOR INDICE 2 EN FUNCIÓN AL INDICE OBTENIDO SEGÚN EL ITEM STRING DE ARRAY STRING Reels
+	
+	
+	totalScore = VALOR INDICE 1 EN FUNCIÓN AL INDICE OBTENIDO SEGÚN EL ITEM STRING DE ARRAY STRING Reels
+	
+	
+	Devolver totalScore
+	
+			
+Fin Función
+
+
+
+
