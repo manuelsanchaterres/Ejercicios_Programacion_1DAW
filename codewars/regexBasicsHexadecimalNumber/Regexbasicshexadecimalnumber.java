111336358a2d362f1c848668728a0ded49deb234
manuelsanchaterres <manuelsanchaterres@gmail.com>
2024-01-26 21:05:25 +0100

    Regexbasicshexadecimalnumber full test suite passed

--- codewars/regexBasicsHexadecimalNumber/Regexbasicshexadecimalnumber.java
+++ codewars/regexBasicsHexadecimalNumber/Regexbasicshexadecimalnumber.java
@@ -6,17 +6,15 @@ public class Regexbasicshexadecimalnumber {
     
     public static void main(String[] args) {
         
-        // isHexNumber("0xB90AFC9879");
-        // isHexNumber("2");
-        isHexNumber("");
-        // isHexNumber("0x");
+        isHexNumber("0X1e5");
     }
 
     public static boolean isHexNumber(String s) {
         // your code goes here
 
+        System.out.println(s);
         if (s.equals("")) return false;
         
-        return Pattern.matches("(^(0x|[0-9A-Fa-f])[0-9A-Fa-f]+)||[0-9A-Fa-f]", s);
+        return Pattern.matches("(^(0x|0X|[0-9A-Fa-f])[0-9A-Fa-f]+)||[0-9A-Fa-f]", s);
     }
 }
