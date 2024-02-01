49086022c111e295f7885bafbf6a5c23e5eb5cc1
manuelsanchaterres <manuelsanchaterres@gmail.com>
2024-02-01 02:37:14 +0100

    Productmaximumsarray codewars kata updated aplying bubble sort method

--- codewars/productMaximumsArray/Productmaximumsarray.java
+++ codewars/productMaximumsArray/Productmaximumsarray.java
@@ -5,13 +5,28 @@ import java.util.Arrays;
 public class Productmaximumsarray {
 
     public static void main(String[] args) {
-        maxProduct(new int[]{14,29,-28,39,-16,-48}, 4);
+        maxProduct(new int[]{13,12,-27,-302,25,37,133,155,-14}, 5);
     }
 
     public static long maxProduct(int[] numbers, int sub_size){
 
-        System.out.println(numbers + ", " + sub_size);
-        Arrays.sort(numbers);
+        int temp;
+
+        // Arrays.sort(numbers);
+
+        //BUBLE ARRAY SORT METHOD
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i+1; j < numbers.length; j++) {
+                
+                if (numbers[i] > numbers[j]){
+
+                    temp = numbers[j];
+                    numbers[j] = numbers[i];
+                    numbers[i] = temp;
+                }
+            }
+        }
         long maxProduct = 1;
         for (int i = numbers.length - 1; i >= numbers.length-sub_size ; i-- ){
 
