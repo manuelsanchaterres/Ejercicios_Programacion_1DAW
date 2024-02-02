763e8127584a9a2e112fb2c3f7930735115d2b39
manuelsanchaterres <manuelsanchaterres@gmail.com>
2024-02-02 20:18:57 +0100

    GoodEvils codewars kata all test passed

--- codewars/fruitMachine/Fruitmachine.java
+++ codewars/fruitMachine/Fruitmachine.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 class Fruitmachine {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
