276503655e6867238b584d9d42dfe8769be38520
manuelsanchaterres <manuelsanchaterres@gmail.com>
2024-02-12 20:53:17 +0100

    codewars twelveTests test passed, check for the rest of tests

---
+++ codewars/cuckooClock/CuckooClock.java
@@ -0,0 +1,65 @@
+package cuckooClock;
+
+public class CuckooClock {
+
+    public static void main(String[] args) {
+        System.out.println(cuckooClock("7:22", 1));
+        // System.out.println(cuckooClock(inputTime+","+chimes));
+    }
+    public static String cuckooClock(String inputTime, int chimes) {
+        String[] inputTimeArray = inputTime.split(":");
+        int hour = Integer.valueOf(inputTimeArray[0]);
+        int minutes = Integer.valueOf(inputTimeArray[1]);
+        int currentChimes = 0;
+        String hourString = "";
+        String minutesString = "";
+        if (!(hour >= chimes && minutes == 0)){
+
+            currentChimes += (60-minutes)/15;
+
+            while (currentChimes < chimes){
+                hour++;
+                if (hour > 12){
+
+                    hour %= 12;
+                }  
+                currentChimes += (hour + 3);
+                       
+            }
+
+            int excedingChimes = currentChimes - chimes;
+            
+            if (excedingChimes > 3){
+                if (excedingChimes - 3 < hour-- && minutes != 0){
+                    minutes = 0;
+                }
+                // else if (excedingChimes - 3 > hour-- && minutes == 0){
+
+                //     minutes = (excedingChimes -3 -hour)*15;
+                // }
+            } else {
+                minutes = ((3- excedingChimes)*15);
+            }
+
+            if (hour < 10){
+
+                hourString = "0"+hour;
+            }else{
+
+                hourString = ""+hour;
+            }
+
+            if (minutes < 10){
+
+                minutesString = "0"+minutes;
+            } else {
+
+                minutesString = ""+minutes;
+            }
+
+            inputTime = hourString+":"+minutesString;
+        }
+
+        return inputTime;
+    }
+}
