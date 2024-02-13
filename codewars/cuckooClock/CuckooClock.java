d16546627ca3950523ade58a00169a7db040184b
manuelsanchaterres <manuelsanchaterres@gmail.com>
2024-02-13 13:24:10 +0100

    cuckooClock kata all tests completed

--- codewars/cuckooClock/CuckooClock.java
+++ codewars/cuckooClock/CuckooClock.java
@@ -3,7 +3,7 @@ package cuckooClock;
 public class CuckooClock {
 
     public static void main(String[] args) {
-        System.out.println(cuckooClock("7:22", 1));
+        System.out.println(cuckooClock("10:00", 13));
         // System.out.println(cuckooClock(inputTime+","+chimes));
     }
     public static String cuckooClock(String inputTime, int chimes) {
@@ -13,12 +13,12 @@ public class CuckooClock {
         int currentChimes = 0;
         String hourString = "";
         String minutesString = "";
-        if (!(hour >= chimes && minutes == 0)){
-
+        if (hour < chimes && minutes != 0 || hour > chimes && minutes != 0){
             currentChimes += (60-minutes)/15;
-
             while (currentChimes < chimes){
+
                 hour++;
+
                 if (hour > 12){
 
                     hour %= 12;
@@ -30,35 +30,63 @@ public class CuckooClock {
             int excedingChimes = currentChimes - chimes;
             
             if (excedingChimes > 3){
-                if (excedingChimes - 3 < hour && minutes != 0){
+                if (excedingChimes - 3 <= hour && minutes != 0){
                     minutes = 0;
                 }
-                // else if (excedingChimes - 3 > hour-- && minutes == 0){
 
-                //     minutes = (excedingChimes -3 -hour)*15;
-                // }
             } else {
                 minutes = ((3- excedingChimes)*15);
             }
 
-            if (hour < 10){
+        }else if (hour < chimes && minutes == 0){
+            currentChimes += hour;
+            while (currentChimes < chimes){
 
-                hourString = "0"+hour;
-            }else{
+                hour++;
 
-                hourString = ""+hour;
+                if (hour > 12){
+
+                    hour %= 12;
+                }  
+                currentChimes += (hour + 3);
+                       
             }
 
-            if (minutes < 10){
+            int excedingChimes = currentChimes - chimes;
+            
+            // if (excedingChimes > hour){
+                int differencefromHourEarlier = excedingChimes - hour - 3;
+                if (differencefromHourEarlier < 0){
 
-                minutesString = "0"+minutes;
-            } else {
+                    if (-differencefromHourEarlier <= 3){
 
-                minutesString = ""+minutes;
-            }
+                        hour--;
+                        minutes += 15 *(-differencefromHourEarlier);
+        
+                    } 
 
-            inputTime = hourString+":"+minutesString;
+                }
+            // }
+
+           
         }
+        if (hour < 10){
+
+            hourString = "0"+hour;
+        }else{
+
+            hourString = ""+hour;
+        }
+
+        if (minutes < 10){
+
+            minutesString = "0"+minutes;
+        } else {
+
+            minutesString = ""+minutes;
+        }
+
+        inputTime = hourString+":"+minutesString;
 
         return inputTime;
     }
