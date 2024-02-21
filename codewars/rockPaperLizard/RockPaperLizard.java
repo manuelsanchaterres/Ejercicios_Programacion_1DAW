a0b367c88dcb2be9a9fe093efeaef910dd74aaa6
manuelsanchaterres <manuelsanchaterres@gmail.com>
2024-02-21 14:15:39 +0100

    RockPaperLizard kata all test passed

---
+++ codewars/rockPaperLizard/RockPaperLizard.java
@@ -0,0 +1,26 @@
+package rockPaperLizard;
+import java.util.Arrays;
+
+public class RockPaperLizard {
+    public static void main(String[] args) {
+        rpsls("rock", "lizard");
+    }
+    
+    static String rpsls(String player1, String player2){
+        String[] player1Victories = {"scpa", "paro","roli","lisp","spsc", "scli", "lipa", "pasp","spro","rosc"};
+        String[] player2Victories = {"pasc", "ropa","liro","spli","scsp", "lisc", "pali", "sppa","rosp","scro"};
+        String newString = player1.substring(0,2)+player2.substring(0,2);
+        System.out.println(player1+","+player2);
+        System.out.println(newString);
+        if (Arrays.asList(player1Victories).contains(newString)){
+
+            return "Player 1 Won!";
+        }else if (Arrays.asList(player2Victories).contains(newString)){
+
+            return "Player 2 Won!";
+        } else {
+
+            return "Draw!";
+        }
+    }
+}
